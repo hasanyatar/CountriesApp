b51ca6f81102aefc79d78707502a2bd30d82d455
hsnytr99 <hsnytr99@gmail.com>
2022-08-28 18:22:12 +0300

    Add navigations and fragments
    Added feed and country fragments on the navigation

---
+++ app/src/main/java/com/example/countriesapp/view/CountryFragment.kt
@@ -0,0 +1,36 @@
+package com.example.countriesapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.countriesapp.R
+
+
+class CountryFragment : Fragment() {
+    private var countryUuid = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let{
+            countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
+        }
+    }
+
+
+
+
+
+}
